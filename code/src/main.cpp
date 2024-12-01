46e24076bdf08df8f392b7399cebee069c345236
alfredos06 <famdahlin4@gmail.com>
2024-12-01 20:33:47 +0100

    testing pwm

--- code/src/main.cpp
+++ code/src/main.cpp
@@ -1,19 +1,30 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+int dutyCycle = 0;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+
+  Serial.begin(9600);
+
+  pinMode(3, OUTPUT);
+  pinMode(11, OUTPUT);
+  //TCCR2A = _BV(COM2A1) | _BV(COM2B1) | _BV(WGM21) | _BV(WGM20);
+  TCCR2A = B01100011;
+  //TCCR2B = _BV(WGM22) | _BV(CS22) | _BV(CS21) | _BV(CS20);
+  TCCR2B = B00001111;
+  OCR2A = 200;
+  OCR2B = 50;
+  
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  analogWrite(11, 20);
+
+  int x = analogRead(A0);
+
+  dutyCycle = map(x, 0, 1023, 0, 219);
+
+  //OCR2B = dutyCycle;
+
+
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
