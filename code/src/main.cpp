31d87dca5f9bcb07d975a62a86fd006970733873
alfredos06 <famdahlin4@gmail.com>
2024-12-07 19:30:11 +0100

    PWM finalized?

--- code/src/main.cpp
+++ code/src/main.cpp
@@ -7,23 +7,38 @@ void setup() {
   Serial.begin(9600);
 
   pinMode(3, OUTPUT);
+  pinMode(10, OUTPUT);
+  pinMode(9, OUTPUT);
+
   pinMode(11, OUTPUT);
-  //TCCR2A = _BV(COM2A1) | _BV(COM2B1) | _BV(WGM21) | _BV(WGM20);
-  TCCR2A = B01100011;
-  //TCCR2B = _BV(WGM22) | _BV(CS22) | _BV(CS21) | _BV(CS20);
-  TCCR2B = B00001111;
-  OCR2A = 200;
-  OCR2B = 50;
+
   
+  TCCR1A = B10100011;
+  TCCR1B = B00010101;
+
+
+  OCR1A = 222; //FREQ
+  OCR1B = 22; //Duty-Cycle
+  
+ 
+  TCCR2A = B00100011;
+  TCCR2B = B00001111;
+
+  OCR2A = 222;
+  OCR2B = 20;
+ 
+
+  
+
 }
 
 void loop() {
 
   int x = analogRead(A0);
 
-  dutyCycle = map(x, 0, 1023, 0, 219);
+  dutyCycle = map(x, 0, 1023, 0, 222);
 
-  //OCR2B = dutyCycle;
+  OCR2B = dutyCycle;
 
 
 }
