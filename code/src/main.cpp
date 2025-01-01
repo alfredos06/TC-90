b74f86ab2db3f70d83d4e18c9f9b7d0e97296264
alfredos06 <famdahlin4@gmail.com>
2025-01-02 00:05:35 +0100

    Experimented

--- code/src/main.cpp
+++ code/src/main.cpp
@@ -1,45 +1,79 @@
 #include <Arduino.h>
 
 int dutyCycle = 0;
+// volatile byte fast = LOW;
+
+
+
+
+  //NOTE: Use another timer which is creates the 70Hz dither which then trigers fast PWM.
+
+
+void fast() {
+
+  if (digitalRead(3) == LOW) {
+    OCR2A = 222;
+    OCR2B = 111;
+  }
+  else {
+    OCR2A = 50;
+    OCR2B = 25; 
+  }
+  
+}
 
 void setup() {
 
   Serial.begin(9600);
 
-  pinMode(3, OUTPUT);
-  pinMode(10, OUTPUT);
-  pinMode(9, OUTPUT);
+  pinMode(3, OUTPUT);   //Drive signal
+  pinMode(10, OUTPUT);  //Dither
 
-  pinMode(11, OUTPUT);
-
-  
-  TCCR1A = B10100011;
-  TCCR1B = B00010101;
+  attachInterrupt(digitalPinToInterrupt(3), fast, CHANGE);
 
 
-  OCR1A = 222; //FREQ
-  OCR1B = 22; //Duty-Cycle
+
+  //TCCR1A = B10100011;
+  //TCCR1B = B00010101;
+
+
+  //OCR1A = 222; //FREQ
+  //OCR1B = 22; //Duty-Cycle
   
  
   TCCR2A = B00100011;
   TCCR2B = B00001111;
 
+  //OCR2A = 222;
+  //OCR2B = 111;
+
   OCR2A = 222;
-  OCR2B = 20;
- 
-
-  
-
+  OCR2B = 111;
 }
 
 void loop() {
 
+  /*
+
   int x = analogRead(A0);
 
-  dutyCycle = map(x, 0, 1023, 0, 222);
+  dutyCycle = map(x, 0, 1023, 0, 255);
 
-  OCR2B = dutyCycle;
+  Serial.println(dutyCycle);
+  //OCR2B = dutyCycle;
 
+  */
+ 
+  /*
+  digitalWrite(3, HIGH);
+  analogWrite(3, dutyCycle);
+  delay(10);
+  digitalWrite(3, LOW);
+  delay(10);
+  */
+
+    OCR2A = 222;
+    OCR2B = 111;
 
 }
 
